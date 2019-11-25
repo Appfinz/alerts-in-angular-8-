1db241456a0f81ba15b69235e2c2fc33937b8128
Appfinz <58131218+Appfinz@users.noreply.github.com>
2019-11-25 13:58:24 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AlertModule } from './_alert';
+import { appRoutingModule } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { MultiAlertsComponent } from './multi-alerts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        AlertModule,
+        appRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        MultiAlertsComponent
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
